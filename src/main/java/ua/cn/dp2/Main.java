43517e76ec91d4b5aa74ebeaae40338a7594eea0
Sviatoslav Romanenko <romanenkos@gmail.com>
2012-03-29 17:49:35 +0300

    rename fo call-a-doctor

---
+++ src/main/java/ua/cn/dp2/Main.java
@@ -0,0 +1,15 @@
+package ua.cn.dp2;
+
+/**
+ * Date: 29.03.12
+ */
+public class Main {
+    
+    
+    public static void main(String ... args){
+        System.out.println("Hello world");
+        System.exit(0);
+    }
+}
+
+
