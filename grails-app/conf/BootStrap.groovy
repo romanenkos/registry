c68a7aedfaa4c2ddaed0aa00d0e8587fe36a9f1b
romanenkos <romanenkos@gmail.com>
2012-06-18 20:53:54 +0300

    Address and District

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,10 +1,28 @@
 import registry.Doctor
+import registry.District
+import registry.Address
 
 class BootStrap {
 
     def init = { servletContext ->
+
+        List<District> districts;
         3.times {
             new Doctor(firstName: "Fname${it}", middleName: "MName${it}", lastName: "LName${it}").save()
+
+            District d = new District(number: "${it}")
+            Set<Address> addresses = new HashSet<Address>();
+            3.times { it1 ->
+                Address a = new Address(district: d, street: "D${it}-Street${it1}", number: "D${it}-N${it1}" )
+                addresses.add(a);
+            }
+
+            d.addresses = addresses
+            if (!d.save()) {
+                d.errors.each { er ->
+                    println er
+                }
+            }
         }
     }
 
---
+++ grails-app/domain/registry/Address.groovy
@@ -0,0 +1,13 @@
+package registry
+
+class Address {
+
+    String street
+    String number
+    District district
+
+
+    static constraints = {
+
+    }
+}
