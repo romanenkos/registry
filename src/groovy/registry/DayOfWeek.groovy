ffd897071d2df7746bcd67f869cf276ea7f55b36
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2012-06-25 20:56:07 +0300

    Utility method to find start of current week

--- src/groovy/registry/DayOfWeek.groovy
+++ src/groovy/registry/DayOfWeek.groovy
@@ -2,13 +2,34 @@ package registry
 
 import org.apache.commons.lang.time.DateUtils
 
-enum DayOfWeek {
-    MON, TUE, WED, THU, FRI, SAT, SUN
+import static java.util.Calendar.getInstance
+import static java.util.Calendar.DAY_OF_WEEK
+import static java.util.Calendar.WEDNESDAY
+import static java.util.Calendar.MONDAY
+import static org.apache.commons.lang.time.DateUtils.truncate
+import static java.util.Calendar.DAY_OF_WEEK
+
+public enum DayOfWeek {
+    MON(Calendar.MONDAY), TUE(Calendar.TUESDAY), WED(Calendar.WEDNESDAY), THU(Calendar.THURSDAY), FRI(Calendar.FRIDAY), SAT(Calendar.SATURDAY), SUN(Calendar.SUNDAY)
+
+    private int dayOfWeek
+
+    DayOfWeek(int dayOfWeek) {
+        this.dayOfWeek = dayOfWeek
+    }
+
+    int toCalendarDayOfWeek() {
+        return dayOfWeek
+    }
 
     static Date startDateOfCurrentWeek() {
-        Calendar cnd = Calendar.getInstance()
-        cnd.setFirstDayOfWeek(Calendar.MONDAY)
-        cnd.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
-        return DateUtils.truncate(cnd.time, Calendar.DATE)
+        return dateForDayOfWeek(MON)
+    }
+
+    static Date dateForDayOfWeek(DayOfWeek dayOfWeek) {
+        Calendar cnd = getInstance()
+        cnd.setFirstDayOfWeek(MONDAY)
+        cnd.set(DAY_OF_WEEK, dayOfWeek.toCalendarDayOfWeek())
+        return truncate(cnd.time, Calendar.DATE)
     }
 }
