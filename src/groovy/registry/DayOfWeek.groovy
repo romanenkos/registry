e936fc990014b7b363912523884d27cbf8fd166e
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2012-06-25 20:38:40 +0300

    Utility method to find start of current week

--- src/groovy/registry/DayOfWeek.groovy
+++ src/groovy/registry/DayOfWeek.groovy
@@ -1,4 +1,14 @@
 package registry
+
+import org.apache.commons.lang.time.DateUtils
+
 enum DayOfWeek {
     MON, TUE, WED, THU, FRI, SAT, SUN
+
+    static Date startDateOfCurrentWeek() {
+        Calendar cnd = Calendar.getInstance()
+        cnd.setFirstDayOfWeek(Calendar.MONDAY)
+        cnd.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+        return DateUtils.truncate(cnd.time, Calendar.DATE)
+    }
 }
