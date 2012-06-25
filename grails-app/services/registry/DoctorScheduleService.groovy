9b18a739b46dd2da24b2c078bd7f3a5eaf74fe2e
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2012-06-25 20:15:11 +0300

    irregular overrides regular schedule items

--- grails-app/services/registry/DoctorScheduleService.groovy
+++ grails-app/services/registry/DoctorScheduleService.groovy
@@ -3,12 +3,19 @@ package registry
 import javax.validation.constraints.NotNull
 
 import static registry.ScheduleItemType.REGULAR
+import static registry.ScheduleItemType.IRREGULAR
+import org.apache.commons.lang.time.DateUtils
 
 class DoctorScheduleService {
     List<Map> findSchedule(@NotNull Doctor doctor) {
         def regular = ScheduleItem.findAllByDoctorAndType(doctor, REGULAR).sort {a, b -> a.day <=> b.day}.collect {
             [day: it.day, room: it.room, workingTime: it.workingTime]
         }
-        return regular
+        def irregular = ScheduleItem.findAllByDoctorAndType(doctor, IRREGULAR)
+        def irregularDays = irregular.collect {it.day}
+
+        return (regular.findAll {!irregularDays.contains(it.day)} + irregular).sort {a, b -> a.day <=> b.day}.collect {
+            [day: it.day, room: it.room, workingTime: it.workingTime]
+        }
     }
 }
--- test/unit/registry/DoctorScheduleServiceTests.groovy
+++ test/unit/registry/DoctorScheduleServiceTests.groovy
@@ -10,23 +10,12 @@ import static registry.DayOfWeek.MON
 import static registry.DayOfWeek.THU
 import static registry.ScheduleItemType.REGULAR
 import static registry.ScheduleItemType.IRREGULAR
+import static registry.DayOfWeek.WED
+import static registry.DayOfWeek.TUE
 
 @TestFor(DoctorScheduleService)
 @Mock([Doctor, ScheduleItem])
 class DoctorScheduleServiceTests {
-
-    @Test
-    void whenFindScheduleThenConsiderOnlyRegularItems() {
-        //given
-        def d = new Doctor(firstName: "fname", lastName: "lname", middleName: "mname")
-                .addToScheduleItems(new ScheduleItem(day: FRI, type: REGULAR))
-                .addToScheduleItems(new ScheduleItem(day: MON, type: IRREGULAR)).save()
-        //when
-        def schedule = service.findSchedule(d)
-        //then
-        assert schedule.size() == 1
-        assert schedule[0].day == FRI
-    }
 
     @Test
     void whenFindScheduleThenOrderedByDayOfWeek() {
@@ -39,5 +28,25 @@ class DoctorScheduleServiceTests {
         def schedule = service.findSchedule(d)
         //then
         assert [MON, THU, FRI] == schedule.collect{it.day}
+    }
+
+    @Test
+    void whenFindScheduleThenOverrideWithIrregular() {
+        //given
+        println ScheduleItem.count()
+        def d = new Doctor(firstName: "fname", lastName: "lname", middleName: "mname")
+                .addToScheduleItems(new ScheduleItem(day: MON, type: REGULAR, workingTime: "09:00 - 17:00"))
+                .addToScheduleItems(new ScheduleItem(day: TUE, type: REGULAR, workingTime: "09:00 - 17:00"))
+                .addToScheduleItems(new ScheduleItem(day: WED, type: REGULAR, workingTime: "09:00 - 17:00"))
+                .addToScheduleItems(new ScheduleItem(day: THU, type: REGULAR, workingTime: "09:00 - 17:00"))
+                .addToScheduleItems(new ScheduleItem(day: FRI, type: REGULAR, workingTime: "09:00 - 17:00"))
+                .addToScheduleItems(new ScheduleItem(day: MON, type: IRREGULAR, workingTime: "DAY OFF"))
+                .addToScheduleItems(new ScheduleItem(day: WED, type: IRREGULAR, workingTime: "DAY OFF")).save()
+        //when
+        def schedule = service.findSchedule(d)
+        //then
+        assert schedule.size() == 5
+        assert schedule[0].workingTime == "DAY OFF"
+        assert schedule[2].workingTime == "DAY OFF"
     }
 }
